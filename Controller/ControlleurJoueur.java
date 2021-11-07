32b518e1e4f5be7ba65e22ddc3982db0812c2248
Arnaud Curie <60758893+Arttys1@users.noreply.github.com>
2021-11-07 16:06:36 +0100

    Amélioration des déplacements du joueur

---
+++ Controller/ControlleurJoueur.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thebindingofalice.Controller;
+
+import thebindingofalice.Metier.joueur.DirectionDeplacement;
+import thebindingofalice.Metier.joueur.Joueur;
+
+/**
+ *
+ * @author Arnaud
+ */
+public class ControlleurJoueur extends Observable{
+    private final Joueur joueur;
+
+    public ControlleurJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    public void bouger(DirectionDeplacement dir)
+    {
+        joueur.Bouger(dir);
+    }
+    
+    public void sArreter()
+    {
+        joueur.sArreter();
+    }
+    
+    public void evoluer(double pas)
+    {
+        joueur.evoluer(pas);
+        Notify("joueur");
+    }
+}
