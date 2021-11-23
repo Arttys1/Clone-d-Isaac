c97ce6db25c4a7d00c925179d53cc104bdb78307
Arnaud Curie <60758893+Arttys1@users.noreply.github.com>
2021-11-23 19:44:29 +0100

    Correction des problèmes de fusion

--- src/thebindingofalice/Metier/objet/obstacle/Mur.java
+++ src/thebindingofalice/Metier/objet/obstacle/Mur.java
@@ -4,6 +4,10 @@ import thebindingofalice.Metier.Coordonnee;
 import thebindingofalice.Metier.Hitbox;
 import thebindingofalice.Metier.ICollision;
 import thebindingofalice.Metier.Partie;
+import thebindingofalice.Metier.joueur.Joueur;
+import thebindingofalice.Metier.niveau.carte.Generateur.CaseMur;
+import thebindingofalice.Metier.niveau.carte.salle.DirectionSalle;
+import thebindingofalice.Metier.niveau.carte.salle.Salle;
 import thebindingofalice.Metier.objet.TypeObjet;
 
 /**
@@ -11,12 +15,37 @@ import thebindingofalice.Metier.objet.TypeObjet;
  */
 public class Mur extends Obstacle {
     private final Hitbox hitbox;
-
+    private DirectionSalle dir = null;
+    
     public Mur(Coordonnee c) {
         super(c);
         hitbox = new Hitbox(c.getX(), c.getY(), 50, 50);
     }
-
+    
+    public Mur(Coordonnee c, CaseMur caseMur)
+    {
+        this(c);
+        int colonne = caseMur.getColonne();
+        int ligne = caseMur.getLigne();
+        if(colonne == 0)
+        {
+            dir = DirectionSalle.GAUCHE;
+        }
+        else if(colonne == Salle.getTailleCote() - 1)
+        {
+            dir = DirectionSalle.DROITE;
+        }
+        else if(ligne == 0)
+        {
+            dir = DirectionSalle.HAUT;
+        }
+        else if(ligne == Salle.getTailleCote() - 1)
+        {
+            dir = DirectionSalle.BAS;
+        }
+    }
+    
+    
     @Override
     public TypeObjet getType() {
         return TypeObjet.MUR;
@@ -31,7 +60,24 @@ public class Mur extends Obstacle {
     public void Collision(ICollision o) {
         if(o.EstLeJoueur())
         {
-            Partie.get().GetJoueur().stop();
+            Joueur joueur = Partie.get().GetJoueur();
+            double x = getCoordonnee().getX();
+            double y = getCoordonnee().getY();
+            Coordonnee coord = joueur.getCoordonnee();
+            switch(dir)
+            {
+                case HAUT: coord.setY(hitbox.getY() + hitbox.getHeight());
+                    break;
+                case BAS: coord.setY(hitbox.getY() - 60);   //valeurs en brut car horrible programmeur
+                    break;
+                case GAUCHE: coord.setX(hitbox.getX() + hitbox.getWidth()); 
+                    break;
+                case DROITE: coord.setX(hitbox.getX() - 60);
+                    break;
+                default:
+                    throw new AssertionError(dir.name());
+                
+            }
         }
     }
 
