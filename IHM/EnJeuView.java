f4d1234b5a0c3ac7023a6f20d516be4b30ee912d
Arnaud Curie <60758893+Arttys1@users.noreply.github.com>
2021-11-30 14:17:43 +0100

    Réparation github

---
+++ IHM/EnJeuView.java
@@ -0,0 +1,143 @@
+package thebindingofalice.IHM;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import thebindingofalice.IHM.view.CoeurView;
+import thebindingofalice.IHM.view.JoueurView;
+import thebindingofalice.IHM.view.MurView;
+import thebindingofalice.Metier.Partie;
+import thebindingofalice.Metier.joueur.DirectionDeplacement;
+import thebindingofalice.Metier.Coordonnee;
+import thebindingofalice.Metier.niveau.carte.Generateur.Case;
+import thebindingofalice.Metier.niveau.carte.Generateur.CaseMur;
+import thebindingofalice.Metier.niveau.carte.Generateur.TypeCase;
+import thebindingofalice.Metier.objet.ramassable.Coeur;
+import thebindingofalice.Metier.projectiles.DirectionTir;
+
+
+/**
+ * FXML Controller class
+ * Vue représentant la fenêtre de jeu.
+ * @author Pascaline, Arnaud
+ */
+public class EnJeuView implements Initializable{  
+    private final Partie partie = Partie.get();     //partie de jeu
+    private JoueurView joueurView;    
+    private final GamePane gamePane = GamePane.get();
+    @FXML
+    private AnchorPane background;  //arrière plan
+    @FXML
+    private AnchorPane root;  //racine
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {    
+        joueurView = new JoueurView();  
+        root.getChildren().add(gamePane.getForeground());
+        GamePane.get().addView(joueurView);
+        
+        
+        displaySalle();
+        instancierDesCoeurs();  //A supprimer plus tard
+        boucleDeJeu();
+    }
+    
+    /**
+     * Méthode représentant la boucle de jeu.
+     * Ce n'est pour l'instant qu'une esquisse mais c'est un début.
+     */
+    private void boucleDeJeu() {        
+        AnimationTimer animationTimer = new AnimationTimer()
+        {
+            @Override
+            public void handle(long pas) {      
+                
+                //for some obscurs reasons pas must be equal to 1
+                partie.Evoluer(1);
+                partie.CheckCollides();
+            }            
+        };
+        animationTimer.start();
+    }
+   
+    @FXML
+    public void handleOnKeyPressed(KeyEvent event)
+    {
+        switch(event.getCode())
+        {
+            case Z:  joueurView.bouger(DirectionDeplacement.HAUT); break;
+            case S:  joueurView.bouger(DirectionDeplacement.BAS); break;
+            case Q:  joueurView.bouger(DirectionDeplacement.GAUCHE); break;
+            case D:  joueurView.bouger(DirectionDeplacement.DROITE); break;
+            case I:  joueurView.tirer(DirectionTir.HAUT); break;
+            case K:  joueurView.tirer(DirectionTir.BAS); break;
+            case J:  joueurView.tirer(DirectionTir.GAUCHE); break;
+            case L:  joueurView.tirer(DirectionTir.DROITE); break;
+            default : break;
+        }
+    }
+       
+    @FXML
+    public void handleKeyRelease(KeyEvent evt)
+    {
+        switch(evt.getCode())
+        {
+            case Z:  joueurView.sArreter(DirectionDeplacement.HAUT); break;
+            case S:  joueurView.sArreter(DirectionDeplacement.BAS); break;
+            case Q:  joueurView.sArreter(DirectionDeplacement.GAUCHE); break;
+            case D:  joueurView.sArreter(DirectionDeplacement.DROITE); break;
+            case I:  joueurView.stopTirer(DirectionTir.HAUT); break;
+            case K:  joueurView.stopTirer(DirectionTir.BAS); break;
+            case J:  joueurView.stopTirer(DirectionTir.GAUCHE); break;
+            case L:  joueurView.stopTirer(DirectionTir.DROITE); break;
+            default : break;
+        }
+    }
+    
+    /**
+     * Méthode qui provisoire servant uniqement à montrer l'affichage de trois coeurs
+     */
+    private void instancierDesCoeurs() {
+        for (int i = 0; i < 3; i++) {
+            Coordonnee coor = new Coordonnee(200 + i * 100 , 500);
+            Coeur c = new Coeur(coor);
+            CoeurView coeurView = new CoeurView(c);            
+            GamePane.get().addView(coeurView);
+        }
+    }
+    
+    
+    /**
+     * Méthode permettant d'afficher la salle en court.
+     */
+    private void displaySalle()
+    {  
+        background.getChildren().clear();
+        int size = 60;
+        for (Case c : partie.getNiveauCourant().getSalleCourante().getCases()) {
+            ImageView img = new ImageView(System.getProperty("user.dir") + "/src/thebindingofalice/Images/Salle/" + c.getSprite());
+            int x = 100 + c.getColonne() * size;
+            int y = 20 + c.getLigne() * size;
+            img.setX(x);
+            img.setY(y);
+            img.setFitHeight(size);
+            img.setFitWidth(size);
+            background.getChildren().add(img);
+            if (c.getType() == TypeCase.MUR) {
+                MurView mur = new MurView(new Coordonnee(x, y), (CaseMur)c);
+                GamePane.get().addView(mur);
+            }
+        }
+    }
+
+    
+
+    
+}
