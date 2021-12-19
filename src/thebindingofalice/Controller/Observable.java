5c3827952e01382a09a126d9b2d9fd57ad48ae17
Arnaud Curie <60758893+Arttys1@users.noreply.github.com>
2021-12-19 12:57:00 +0100

    Résolution des problèmes de framerate

--- src/thebindingofalice/Controller/Observable.java
+++ src/thebindingofalice/Controller/Observable.java
@@ -14,10 +14,25 @@ public abstract class Observable {
     }
     
     /**
-     * Méthode permettant de s'enregistrer à ce controlleur. La vue sera averti en cas de chanement du model.
+     * Méthode permettant de s'enregistrer à cet observable. La vue sera averti en cas de changement du model.
      * @param o Observeur à enregistrer.
      */
     public void Register(Observeur o){ obs.add(o); }
+    
+    
+    /**
+     * Méthode permettant de s'enregistrer à cet observable. La vue sera averti en cas de changement du model.
+     * Cette méthode vérifie si l'observeur à déjà été ajouté et ne l'ajoute que s'il n'y est pas déjà.
+     * @param o Observeur à enregistrer.
+     */
+    public void RegisterOnce(Observeur o)
+    {
+        if(!obs.contains(o))
+        {
+            Register(o);
+        }
+    }
+    
     
     /**
      * Méthode, appelé lors d'un changement du model, avertissant toutes les vues du changement.
