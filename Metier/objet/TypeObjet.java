d630bf465128843edb976e1d3994bc8e8607f6ef
Arnaud Curie <60758893+Arttys1@users.noreply.github.com>
2022-01-06 12:28:17 +0100

    Delete Metier directory

--- Metier/objet/TypeObjet.java
+++
@@ -1,7 +0,0 @@
-package thebindingofalice.Metier.objet;
-/**
- * Enumération représentant les différents type d'objet
- */
-public enum TypeObjet {
-    MUR,COEUR,CoeurDroitHUD,CoeurGaucheHUD, ROCHER, CLE, VALEUR, PORTE
-}
